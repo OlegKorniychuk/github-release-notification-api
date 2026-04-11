b48db20372acf9eeb0883580b4f4a24ac7dbd834
OlegKorniychuk <kornoleg576@gmail.com>
2026-04-12 01:11:15 +0300

    feat: remove api-key auth from confirm and unsubscribe endpoints

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,12 @@ import express from 'express';
 import morgan from 'morgan';
 import router from './routes.js';
 import { handleError } from './utils/error-handling/handle-error.js';
-import { requireApiKey } from './auth/api-key.middleware.js';
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use(requireApiKey);
 
 app.use(router);
 
